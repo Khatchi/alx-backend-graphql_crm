1eda8a6fd0a826ec830c807fcd5095553a34e68d
Khatchi <solomon9ice2014@gmail.com>
2025-06-28 23:59:36 +0100

    graphql implemented

--- graphql_crm/crm/schema.py
+++ graphql_crm/crm/schema.py
@@ -1,12 +1,9 @@
 import graphene
 
-
 class Query(graphene.ObjectType):
-    # Define query fields
-
     hello = graphene.String()
 
     def resolve_hello(self, info):
-        return f"Hello, GraphQL!"
-    
+        return "Hello, GraphQL!"
+
 schema = graphene.Schema(query=Query)
